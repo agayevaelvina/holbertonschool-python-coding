cd2b4fb8866f5c51eabdf531b004309e2ee444a8
agayevaelvina <elvinaagyeva@gmail.com>
2025-06-26 18:25:43 +0000

    x

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a Square class with size validation and area computation."""
-
+"""This module defines a Square class with size and area calculation."""
 
 class Square:
     """Represents a square with a private size attribute."""
